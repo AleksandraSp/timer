e00114d3ee4a3d273d8452ff67eb330f6172b40d
AleksandraSp <aleksandranspasojevic@gmail.com>
2016-07-01 18:10:26 +0200

    added New page

--- timer/module/Timer/src/Timer/Controller/TimerCreator.php
+++ timer/module/Timer/src/Timer/Controller/TimerCreator.php
@@ -1,9 +1,15 @@
 <?php
 namespace Timer\Controller;
 
-class TimerCreator {
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class TimerCreator extends AbstractActionController {
   public function indexAction()
   {
+      return new ViewModel(array(
+          'aaa' => 1
+      ));
     //TODO:
     echo 'Form with input fields';
   }
